d88d5be4e90fb3049134529e4b2ea1781b2bcf7b
Verdyon/Mobile-IF633-DL <78303173+Verdyon@users.noreply.github.com>
2021-04-27 16:18:30 +0700

    Add files via upload

---
+++ Week04B_32810/app/src/main/java/umn/ac/id/ThirdActivity.java
@@ -0,0 +1,19 @@
+package umn.ac.id;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+
+    }
+}
